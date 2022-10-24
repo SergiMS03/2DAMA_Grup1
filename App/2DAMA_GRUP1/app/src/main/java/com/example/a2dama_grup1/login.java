a9dd412bcf821b1135775883ce57104a17a4e256
ausias <a20alvlozhue@inspedralbes.cat>
2022-10-24 12:20:21 +0200

    login fix

--- App/2DAMA_GRUP1/app/src/main/java/com/example/a2dama_grup1/login.java
+++ App/2DAMA_GRUP1/app/src/main/java/com/example/a2dama_grup1/login.java
@@ -2,13 +2,28 @@ package com.example.a2dama_grup1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class login extends AppCompatActivity {
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+        button = (Button) findViewById(R.id.registerButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openNewActivity();
+            }
+        });
+    }
+    public void openNewActivity(){
+        Intent intent = new Intent(this, sign_up.class);
+        startActivity(intent);
     }
 }
