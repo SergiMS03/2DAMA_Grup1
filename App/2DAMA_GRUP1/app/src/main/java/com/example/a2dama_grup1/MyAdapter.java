d08b6e4eb6d995e257f113b0890ca379d8076061
SergiMS03 <a19sermelseg@inspedralbes.cat>
2022-11-07 12:54:05 +0100

    RecyclerView implementada, falta acabar

---
+++ App/2DAMA_GRUP1/app/src/main/java/com/example/a2dama_grup1/MyAdapter.java
@@ -0,0 +1,55 @@
+package com.example.a2dama_grup1;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+
+    String data1[];
+    int images[];
+    Context context;
+
+    public MyAdapter(Context ct, String s1[], int img[]){
+        context = ct;
+        data1 = s1;
+        images = img;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.my_row, parent, false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        holder.myText1.setText(data1[position]);
+        holder.myImage.setImageResource(images[position]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return data1.length;
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+
+        TextView myText1;
+        ImageView myImage;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            myText1 = itemView.findViewById(R.id.myText1);
+            myImage = itemView.findViewById(R.id.myImageView);
+        }
+    }
+}
--- App/2DAMA_GRUP1/app/src/main/java/com/example/a2dama_grup1/main_page.java
+++ App/2DAMA_GRUP1/app/src/main/java/com/example/a2dama_grup1/main_page.java
@@ -1,14 +1,27 @@
 package com.example.a2dama_grup1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class main_page extends AppCompatActivity {
 
+    String s1[];
+    int images[] = {R.drawable.a,R.drawable.b,R.drawable.c,R.drawable.d,R.drawable.e,R.drawable.f,R.drawable.g,R.drawable.h};
+    RecyclerView recyclerView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
+        recyclerView = findViewById(R.id.recyclerView);
+
+        s1 = getResources().getStringArray(R.array.ProductTitles);
+
+        MyAdapter myAdapter = new MyAdapter(this, s1, images);
+        recyclerView.setAdapter(myAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
