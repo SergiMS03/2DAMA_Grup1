78c17beab0ac84791b1279a209a7f457f473c24f
SergiMS03 <a19sermelseg@inspedralbes.cat>
2022-11-08 10:20:28 +0100

    Scroll horitzontal Productes

--- App/2DAMA_GRUP1/app/src/main/java/com/example/a2dama_grup1/upload_product.java
+++ App/2DAMA_GRUP1/app/src/main/java/com/example/a2dama_grup1/upload_product.java
@@ -1,8 +1,25 @@
 package com.example.a2dama_grup1;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Date;
 
 import okhttp3.MediaType;
@@ -17,17 +34,121 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class upload_product extends AppCompatActivity {
 
+    private EditText product_name;
+    private EditText price;
+    private EditText stock;
+    private EditText product_description;
+    private ImageView image;
+
     private static Retrofit retrofit;
-    private static String BASE_URL =  "http://192.168.207.154:3000/pushImage";
+    private static String url_pushImage =  "http://192.168.207.154:3000/pushImage";
     Date fecha = new Date();
     String filePath = fecha + ".jpeg";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload_product);
+
+        product_name = (EditText) (findViewById(R.id.editText_nomProducte_upload));
+        price = (EditText) (findViewById(R.id.editText_precio_upload));
+        stock = (EditText) (findViewById(R.id.editText_stock_upload));
+        product_description = (EditText) (findViewById(R.id.editText_descripcio_upload));
+        image = (ImageView) (findViewById(R.id.id_imageView_upload));
+
+    }
+    
+    public boolean onSupportNavigateUp(){
+        onBackPressed();
+        return true;
+    }
+
+    public void displayToast (String message){
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+    }
+
+    public void clickUploadImage(View view){
         uploadImage();
     }
 
+    public void clickUploadProduct(View view){
+        String HOST = "http://192.168.207.154:3000/uploadProduct/"+product_name.getText()+"/"+price.getText()+"/"
+                +stock.getText()+"/"+product_description.getText()+"/"+filePath;
+        new productToServer().execute(HOST);
+    }
+
+    public class productToServer extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... strings) {
+            Log.i("LOGINFO", "do in Background: !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+
+            return dades(strings[0]);
+        }
+
+        private String dades(String queryString){
+            HttpURLConnection con = null;
+            BufferedReader reader = null;
+            String result = null;
+
+            try{
+                Log.i("LOGINFO", "dades: !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+
+                String url = queryString;
+                Uri builtURI = Uri.parse(url).buildUpon().build();
+                URL requestURL = new URL(builtURI.toString());
+                con = (HttpURLConnection) requestURL.openConnection();
+                con.setRequestMethod("GET");
+                con.connect();
+
+                InputStream inputStream = con.getInputStream();
+
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+                StringBuilder builder = new StringBuilder();
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    builder.append(line);
+                    builder.append("\n");
+                }
+                if (builder.length() == 0) {
+                    return null;
+                }
+
+                result = builder.toString();
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }finally{
+                if (con != null)
+                    con.disconnect();
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        protected void onPostExecute(String s){
+            super.onPostExecute(s);
+            try {
+                if(s.equals("0\n")){
+                    displayToast("Upload successful");
+                }
+                else{
+                    displayToast("Alguna dada no és correcte");
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
     private void uploadImage() {
         File file = new File(filePath);
 
@@ -56,7 +177,7 @@ public class upload_product extends AppCompatActivity {
     public static Retrofit getRetrofit(){
         OkHttpClient okHttpClient = new OkHttpClient.Builder().build();
         if(retrofit == null) {
-            retrofit = new Retrofit.Builder().baseUrl(BASE_URL).
+            retrofit = new Retrofit.Builder().baseUrl(url_pushImage).
                     addConverterFactory(GsonConverterFactory.create()).client(okHttpClient).build();
         }
         return retrofit;
