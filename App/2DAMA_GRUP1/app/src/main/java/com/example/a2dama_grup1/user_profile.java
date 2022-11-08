bc286f9d2aaa05eba439e96ab51aa21c31f48025
moi190593 <a21moigarped@inspedralbes.cat>
2022-11-08 10:02:07 +0100

    android upload product (pdt probar-ho)

--- App/2DAMA_GRUP1/app/src/main/java/com/example/a2dama_grup1/user_profile.java
+++ App/2DAMA_GRUP1/app/src/main/java/com/example/a2dama_grup1/user_profile.java
@@ -2,7 +2,9 @@ package com.example.a2dama_grup1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class user_profile extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class user_profile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
+    }
+
+    public void clickUpload (View view){
+        Intent intent = new Intent(user_profile.this, upload_product.class );
+        startActivity(intent);
     }
 }
